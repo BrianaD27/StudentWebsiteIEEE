76d459070fdd3bf81485dd1279a1a157808bfc75
Briana Davis <brianad570@gmail.com>
2026-02-16 17:41:05 -0500

    Init Commit

---
+++ IEEEWebsite/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='font-extrabold'>IEEE Student Website</h1>
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
